4715bf061788a984ec76442006db34963382e162
Alexandr <sparkygino@gmail.com>
2025-08-28 17:21:42 +0300

    start landing

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <div className="container">
+          <div className="header-container">
+            <div className="header-container_item">
+                Logo
+            </div>
+            <div className="header-container_item">
+                <Link className="header-container_item-link">О компании</Link>
+                <Link className="header-container_item-link">Услуги</Link>
+                <Link className="header-container_item-link">Кейсы</Link>
+                <Link className="header-container_item-link">Блог</Link>
+                <Link className="header-container_item-link">Контакты</Link>
+
+            </div>
+            <div className="header-container_item">
+                <button className="action-button">
+                    Обсудить проект
+                </button>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
