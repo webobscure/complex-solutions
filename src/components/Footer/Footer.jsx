3c5a1eb1d78f594105f0f9ea78377e6f2ae008ca
Vladimir Lopatin <107243868+21Trew@users.noreply.github.com>
2025-08-29 22:29:55 +0300

    обновил footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,38 @@
-import React from "react";
 import "./Footer.css";
+
+import Container from "../elements/Container/Container";
+
 export default function Footer() {
 	return (
 		<footer className="footer-container ">
-			<div className="footer-block container">
-				<div className="footer-block__left">
-					<h2>Юридический адрес</h2>
-					<p>
-						606060, г. Москва, ул.
-						<br /> Орджоникидзе, д. 38
-					</p>
-				</div>
-				<div className="footer-block__right">
-					<div className="footer-block__right-item">
-						<h2>Контактный телефон</h2>
-						<p>+8 (800) 299-8139</p>
+			<Container>
+				<div className="footer-block">
+					<div className="footer-block__left">
+						<h2>Юридический адрес</h2>
+
+						<p>
+							606060, г. Москва, ул.
+							<br /> Орджоникидзе, д. 38
+						</p>
 					</div>
-					<div className="footer-block__right-item">
-						<h2>Email</h2>
-						<p>example@company.ru</p>
+
+					<div className="footer-block__right">
+						<div className="footer-block__right-item">
+							<h2>Контактный телефон</h2>
+
+							<p>+8 (800) 299-8139</p>
+						</div>
+
+						<div className="footer-block__right-item">
+							<h2>Email</h2>
+
+							<p>example@company.ru</p>
+						</div>
 					</div>
 				</div>
-			</div>
-			<span>Политика обработки персональных данных</span>
+
+				<p className="footer-policy">Политика обработки персональных данных</p>
+			</Container>
 		</footer>
 	);
 }
